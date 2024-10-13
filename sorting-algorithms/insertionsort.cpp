921afe15e4c822f4b98bd511d143a2a2176f0e59
LachezarKunov <96826419+LachezarKunov@users.noreply.github.com>
2024-10-13 15:04:50 +0300

    add insertionsort.cpp

---
+++ sorting-algorithms/insertionsort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template<class T>
+void insertionSort(vector<T>& v) 
+{
+	for (size_t i = 1; i < v.size(); i++)
+	{
+		T temp = v[i];
+		int j = i - 1;
+
+		while (j >= 0 && v[j] > temp) 
+		{
+			v[j + 1] = v[j];
+			j--;
+		}
+
+		v[j + 1] = temp;
+	}
+}
+
+int main()
+{
+  vector<int> vector{ 4,3,2,1,6 };
+	
+	insertionSort(vector);
+
+	for (size_t i = 0; i < vector.size(); i++)
+	{
+		std::cout << vector[i];
+	}
+}
