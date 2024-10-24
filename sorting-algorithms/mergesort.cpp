c1b18bb1af934c8cfebb9b3cef8d5cb8651e6357
LachezarKunov <96826419+LachezarKunov@users.noreply.github.com>
2024-10-24 21:34:11 +0300

    Add MergeSort

---
+++ sorting-algorithms/mergesort.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+template<class T>
+void Merge(T* firstArr, size_t firstSize, T* secondArr, size_t secondSize, T* resultArr)
+{
+    size_t resultIter = 0;
+    size_t firstIter = 0;
+    size_t secondIter = 0;
+
+    while (firstIter < firstSize && secondIter < secondSize)
+        resultArr[resultIter++] = (firstArr[firstIter] <= secondArr[secondIter] ? firstArr[firstIter++] : secondArr[secondIter++]);
+
+    while (firstIter < firstSize)
+        resultArr[resultIter++] = firstArr[firstIter++];
+
+    while (secondIter < secondSize)
+        resultArr[resultIter++] = secondArr[secondIter++];
+}
+
+template<class T>
+void MergeSortStep(T* pArr, size_t Size, T* pBuffer) 
+{
+    if (Size <= 1)
+        return;
+
+    size_t middle = Size / 2;
+
+    MergeSortStep(pArr, middle, pBuffer);
+
+    MergeSortStep(pArr + middle, Size - middle, pBuffer + middle);
+
+    Merge(pArr, middle, pArr + middle, Size - middle, pBuffer);
+
+    for (size_t i = 0; i < Size; i++)
+    {
+        pArr[i] = pBuffer[i];
+    }
+}
+
+template<class T>
+void MergeSort(T* pArr, size_t Size) 
+{
+    if (!pArr || Size == 0)
+        return;
+
+    T* pBuffer = new T[Size];
+
+    MergeSortStep(pArr, Size, pBuffer);
+
+    delete[] pBuffer;
+}
+
+const int SIZE = 10;
+
+int main()
+{
+    int arr[SIZE]{ 7,8,9,1,4,3,5,0,-1,-50 };
+
+    MergeSort(arr, SIZE);
+
+    for (size_t i = 0; i < SIZE; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
